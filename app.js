8360164c8322161a3c6f375a82262873bf217409
rachelrc <rachelrchandra@gmail.com>
2021-06-16 19:45:50 +0800

    connect to mongodb

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -14,6 +15,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+	collection.insertOne(req.body, (err, result) => {  
+    if (err) return console.log(err)
+    console.log('saved to database')
 	res.redirect('/thankyou');
 });
 
@@ -28,4 +32,22 @@ app.get('/thankyou', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('This app is running on port' + port);
+});
+
+const MongoClient = require('mongodb').MongoClient;
+
+const CONNECTION_URL = "mongodb+srv://rachelrc:Mn6k7f*3@cluster0.frxw3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DATABASE_NAME = "piccadilly"; // you can change the database name
+var database, collection;
+
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  if(error) throw error;
+
+  database = client.db(DATABASE_NAME);
+  collection = database.collection("newcollection"); // you can change the collection name
+
+  // Start the application after the database connection is ready
+  app.listen(port, () => {
+    console.log('This app is running on port ' + port)
+  });
 });
