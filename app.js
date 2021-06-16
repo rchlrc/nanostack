9de488c39be4d4fa854b178c6af2c6f9c8266dd0
rachelrc <rachelrchandra@gmail.com>
2021-06-16 19:06:00 +0800

    added form and about route

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
   res.render('home');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+app.post('/', (req, res) => {
+	res.render('home');
 });
 
 const port = process.env.PORT || 3000;
