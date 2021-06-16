df600459b211c171670b6bb868d7e85e73ecefa6
rachelrc <rachelrchandra@gmail.com>
2021-06-16 18:01:10 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.set("view engine", "ejs");
+app.get("/", function(req, res){
+  res.render('home');
+});
+app.listen(3000, function()
+  {console.log("Server started on port 3000");}
+);
