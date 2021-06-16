07704581ab1d010cf42e16ef6996939ff7fb789e
rachelrc <rachelrchandra@gmail.com>
2021-06-16 19:49:35 +0800

    remove body parser

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
